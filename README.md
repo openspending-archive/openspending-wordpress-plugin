17d39dea04352fe95fc253bf511b3b5be0e365fc
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2013-09-06 13:25:05 +0000

    Initial version of the OpenSpending WordPress plugin. Includes a demo version of compiled openspendingjs (not officially released yet). Usable by WordPress and supports the treemap visualisation.

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-openspending-wordpress-plugin
-=============================
+# OpenSpending - WordPress plugin
 
-WordPress plugin to make openspendingjs visualisations easy to add to WordPress posts and pages
+WordPress plugin to make openspendingjs visualisations easy to add to WordPress posts and pages.
+
+## Installation
+
+1. Add the openspending directory into your WordPress instance's plugin
+   directory (usually *wp-content/plugins/*).
+2. Go to your admin panel, navigate to plugins, find OpenSpending and
+   activate it.
+
+## How to Use
+
+The OpenSpending WordPress plugin operates using the standard WordPress *shortcodes*, namely:
+
+    [openspending]
+
+Three attributes **must** be defined:
+
+* *type* - The type of visualisation to use
+* *dataset* - The dataset on openspending.org to use
+* *drilldowns* - Dimensions of the dataset to drill down into
+
+If you would like to add a *treemap* for the dimensions *from* and *to* in the [*ukgov-25k-spending* dataset](http://openspending.org/ukgov-25k-spending) on OpenSpending the only thing you need to add to your post or page is:
+
+    [openspending type="treemap" dataset="ukgov-25k-spending" dimensions="from,to"]
+
+## Supported Visualisations
+
+* Treemap (type: "treemap")
---
+++ openspending/openspending.php
@@ -0,0 +1,106 @@
+<?php
+/*
+Plugin Name: OpenSpending
+Plugin URI: http://tryggvib.github.io/openspending-wordpress-plugin/
+Description: Easily add <a href="http://openspending.org" title="OpenSpending - Mapping the Money">OpenSpending</a> visualisations to your blog or pages using the openspending shortcode. This means that you can easily map the money by loading a dataset into <a href="http://openspending.org" title="OpenSpending - Mapping the Money">OpenSpending</a> (if it isn't already there) and dropping a shortcode into your WordPress!
+Author: Open Knowledge Foundation
+Version: 0.1
+Author URI: http://okfn.org/
+License: GPLv2 or later
+*/
+
+/*  Copyright 2013 Open Knowledge Foundation ( http://okfn.org )
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/* OpenSpending plugin class
+   Adds a shortcode handler for [openspending] that needs at least three
+   attributes:
+   * 'type': The type of visualisation
+             e.g. treemap, bubbletree
+   * 'dataset': What dataset on OpenSpending it should visualize,
+                e.g. ukgov-finances-cra
+   * 'drilldowns': Dimensions of the data to drilldown into,
+                   e.g. from,to
+*/
+class OpenSpending {
+
+    // Variable set when shortcode is found to print stylesheets and scripts
+    static $add_style_and_script;
+
+    // Set hooks for this plugin
+    static function init() {
+        // This plugin searches for the openspending shortcode which creates
+        // as visualisation
+        add_shortcode('openspending', array(__CLASS__, 'add_visualisation'));
+
+        // Add triggers to register scripts and print them
+        add_action('init', array(__CLASS__, 'register_style_and_script'));
+        add_action('wp_footer', array(__CLASS__, 'print_style_and_script'));
+    }
+    
+    // Function called when the shortcode is found in a post/page
+    static function add_visualisation($atts) {
+        // Extract the attributes for the shortcode
+        extract( shortcode_atts( array(
+            'type' => null,
+            'dataset' => null,
+            'drilldowns' => null,
+	), $atts ) );
+
+        // Type, Dataset and Drilldowns must be defined
+        if ( $type == null || $dataset == null || $drilldowns == null )
+            return "<emph>Badly formed OpenSpending visualisation</emph>";
+
+        // Set variable that adds style and script to the page/post
+        self::$add_style_and_script = true;
+
+        return "<div class=\"{$type}\" " .
+               "data-dataset=\"{$dataset}\" " .
+               "dataset-drilldowns=\"{$drilldowns}\" >" .
+               "</div>";
+    }
+
+    // Register the stylesheet and javascript we might need to use
+    static function register_style_and_script() {
+        // Register the CSS stylesheet for inclusion in the page if needed
+        wp_register_style('openspending', 
+                          plugins_url('css/openspending.min.css', __FILE__));
+
+        // Register the JS script for inclusion if needed.
+        wp_register_script('openspending',
+                           plugins_url('js/openspending.min.js', __FILE__),
+                           array('jquery'),
+                           null, true);
+    }
+
+    static function print_style_and_script() {
+        // If the we don't have to add the style and script we just return
+        if ( ! self::$add_style_and_script )
+            return;
+
+        // Add the stylesheets and the scripts. This way we add the stylesheet
+        // into the body, not into the header, but we can safely assume that
+        // it takes longer to make a request for the data than it is to parse
+        // and apply the stylesheet
+        wp_print_styles('openspending');
+        wp_print_scripts('openspending');
+    }
+}
+
+// Initialize our plugin class
+OpenSpending::init();
+
+?>
